89f3970e5a80eac71578bb5d4201ecc83298e594
YYnfante <nobu027.99@gmail.com>
2021-05-11 15:48:37 -0400

    +Cols add

--- src/components/home/RootView.jsx
+++ src/components/home/RootView.jsx
@@ -44,27 +44,58 @@ const RootView = () => {
     return (
         <div className="card-columns right p-3">
             <div className="container">
+                
+                
+                
                 <div className="row">
-                    <div className="col-6 col-sm-3">
-                        <div className="card-body" key={1}>
+
+                    {cards.map((xe,xeid) => 
+                        <div className="col-6 col-sm-3" key={xeid}>
+                            <div className="card-body" key={xeid}>
+
+                                <img className="card-img-top border border-dark rounded" 
+                                src={xe.apiImg === "" ? defaultImgCover : xe.apiImg} alt="..."/>
+                                
+                                <div className="card">
+                                    
+                                    
+                                    <h5 className="card-title">{xe.apiName}</h5>
+                                    <div className="card-body">
+                                        <p className="card-text">{xe.apiDescription}</p>
+                                    </div>
+                                    <button className="btn btn-primary">Button</button>
+                                
+                                
+                                
+                                </div>
+
+                            </div>
+                        </div>
+                    )}
+                    
+                    <div className="col-6 col-sm-3" key={cards + 1}>
+                        <div className="card-body" key={cards + 1}>
+                            
                             <img src={defaultImgCover} className="card-img-top border border-dark rounded" alt="..."/>
                             <div className="card">
+                                
                                 <h5 className="card-title">Add a new Api</h5>
                                 <p className="card-text">
                                     {"Add a new Api, it can be any kind of API, not just image API's, try adding something new :)"}
                                 </p>
                                 <i className="btn btn-primary">Add</i>
+                            
                             </div>
+
                         </div>
 
                     </div>
-                    <div className="col-6 col-sm-3">  </div>
-                    <div className="col-6 col-sm-3">.col-6 .col-sm-3</div>
-                    <div className="col-6 col-sm-3">.col-6 .col-sm-3</div>
-                    <div className="col-6 col-sm-3">.col-6 .col-sm-3</div>
-                    <div className="col-6 col-sm-3">.col-6 .col-sm-3</div>
-                    <div className="col-6 col-sm-3">.col-6 .col-sm-3</div>
+
+
                 </div>
+
+
+
             </div>
         </div>
     )
